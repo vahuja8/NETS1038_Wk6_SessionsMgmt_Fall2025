3a507bf8f6897d1b5e5d70e763921f5c839da213
vahuja8 <vahuja8@mycentennial.ca>
2025-10-07 10:47:50 -0400

    Week6-sessionMgmtExample

---
+++ Week3_3ServletSessionExample/src/main/java/com/va/week3/ServletSessions/FirstServlet.java
@@ -0,0 +1,74 @@
+package com.va.week3.ServletSessions;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/* Author - Vivek
+ * Date - Sept 25- 2021
+ * Desc - First Servlet that handles the sessions ..
+ * Provides a way to identify a user across more than one pagerequest or visit to a Web site and to store information about that user. 
+ */
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public FirstServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+
+			String n = request.getParameter("userName");
+			out.print("Welcome " + n);
+
+	/*
+	 * for sessions...		
+	 */
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("userName", n);
+				
+
+			out.print("<a href='SecondServlet'>visit</a>");
+
+			out.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Week3_3ServletSessionExample/src/main/java/com/va/week3/ServletSessions/SecondServlet.java
@@ -0,0 +1,60 @@
+package com.va.week3.ServletSessions;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public SecondServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+
+			HttpSession session = request.getSession(true);
+			
+			String n = (String) session.getAttribute("userName");
+	
+			out.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
